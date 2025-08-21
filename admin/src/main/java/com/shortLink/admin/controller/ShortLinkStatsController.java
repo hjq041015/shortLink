d4a50cd65a5e473db83f664ae0d41157ddeadbfc
zhiyi <huangjiaqi041015@gmail.com>
2025-08-21 17:08:52 +0800

    feat: 开发短链接后管项目调用监控接口

---
+++ admin/src/main/java/com/shortLink/admin/controller/ShortLinkStatsController.java
@@ -0,0 +1,31 @@
+package com.shortLink.admin.controller;
+
+import com.shortLink.admin.common.convention.result.Result;
+import com.shortLink.admin.remote.ShortLinkRemoteService;
+import com.shortLink.admin.remote.dto.req.ShortLinkStatsReqDTO;
+import com.shortLink.admin.remote.dto.resp.ShortLinkStatsRespDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接监控控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class ShortLinkStatsController {
+
+    /**
+     * 后续重构为 SpringCloud Feign 调用
+     */
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
+    };
+
+    /**
+     * 访问单个短链接指定时间内监控数据
+     */
+    @GetMapping("/api/shortLink/admin/v1/stats")
+    public Result<ShortLinkStatsRespDTO> shortLinkStats(ShortLinkStatsReqDTO requestParam) {
+        return shortLinkRemoteService.oneShortLinkStats(requestParam);
+    }
+}
